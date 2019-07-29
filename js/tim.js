e0896200d74be8188a62bb0d91cd10428f8c5f9a
Danbi Kim <danbikim@danbis-air.lan>
2019-07-28 22:26:29 -0400

    tim

---
+++ js/tim.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Popup from "reactjs-popup";
+
+export default () => (
+    <Popup trigger={<button>Tim</button>} position="right center">
+      <div>Popup content here !!</div>
+    </Popup>
+  );
